a26d743095758a4d917b67802e327ad27367deb3
rickauer <116601341+rickauer@users.noreply.github.com>
2022-10-24 18:47:29 -0300

    Update decremento.vhd

--- decremento.vhd
+++ decremento.vhd
@@ -30,10 +30,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity decremento is
-    Port ( cin : in  STD_LOGIC;
-			  a, b : in  STD_LOGIC_vector(3 downto 0);
-           s : out  STD_LOGIC_vector(3 downto 0);
-           cout : out  STD_LOGIC);
+    Port ( cin : in  STD_LOGIC; -- Entrada Carry In
+			  a, b : in  STD_LOGIC_vector(3 downto 0); -- Entrada de Dois Vetores de 4 bits
+           s : out  STD_LOGIC_vector(3 downto 0); -- Saída de um vetor de 4 bits
+           cout : out  STD_LOGIC); -- Saída Carry Out
 
 end decremento;
 architecture Behavioral of decremento is
@@ -42,7 +42,7 @@ architecture Behavioral of decremento is
 	END COMPONENT;
 	SIGNAL t: std_logic_vector(3 downto 1);
 begin
-	FA1: adder PORT MAP('0',a(0),'1',s(0),t(1));
+	FA1: adder PORT MAP('0',a(0),'1',s(0),t(1)); -- Aqui estamos fazendo a soma do vetor "a" com o número -1 para conseguir o decremento
 	FA2: adder PORT MAP(t(1),a(1),'1',s(1),t(2));
 	FA3: adder PORT MAP(t(2),a(2),'1',s(2),t(3));
 	FA4: adder PORT MAP(t(3),a(3),'1',s(3),Cout);
