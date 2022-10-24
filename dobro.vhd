b9e8ffa655203a6ae080a182221d74f2d13ce6dd
rickauer <116601341+rickauer@users.noreply.github.com>
2022-10-24 18:49:21 -0300

    Update dobro.vhd

--- dobro.vhd
+++ dobro.vhd
@@ -30,10 +30,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity dobro is
-    Port ( cin : in  STD_LOGIC;
-			  a, b : in  STD_LOGIC_vector(3 downto 0);
-           s : out  STD_LOGIC_vector(3 downto 0);
-           cout : out  STD_LOGIC);
+    Port ( cin : in  STD_LOGIC; -- Entrada Carry In
+			  a, b : in  STD_LOGIC_vector(3 downto 0); -- Entrada de dois vetores de bits 
+           s : out  STD_LOGIC_vector(3 downto 0); -- Saída de um vetor de 4 bits
+           cout : out  STD_LOGIC); -- Saída Carry Out
 
 end dobro;
 architecture Behavioral of dobro is
@@ -42,7 +42,7 @@ architecture Behavioral of dobro is
 	END COMPONENT;
 	SIGNAL t: std_logic_vector(3 downto 1);
 begin
-	FA1: adder PORT MAP('0',a(0),a(0),s(0),t(1));
+	FA1: adder PORT MAP('0',a(0),a(0),s(0),t(1)); --nesse caso, estamos somando um vetor com ele mesmo, para assim, conseguir o fator 2*a.
 	FA2: adder PORT MAP(t(1),a(1),a(1),s(1),t(2));
 	FA3: adder PORT MAP(t(2),a(2),a(2),s(2),t(3));
 	FA4: adder PORT MAP(t(3),a(3),a(3),s(3),Cout);
