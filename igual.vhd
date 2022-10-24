7f5bac66fc7c925e66fb1a8d59980a4354a109d4
rickauer <116601341+rickauer@users.noreply.github.com>
2022-10-24 18:32:10 -0300

    Update igual.vhd

--- igual.vhd
+++ igual.vhd
@@ -30,16 +30,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity igual is
-    Port ( cin : in  STD_LOGIC;
-			  a, b : in  STD_LOGIC_vector(3 downto 0);
-           s : out  STD_LOGIC_vector(3 downto 0);
-           cout : out  STD_LOGIC);
+    Port ( cin : in  STD_LOGIC; -- aqui recebemos as entradas, sendo cin o Carry In
+			  a, b : in  STD_LOGIC_vector(3 downto 0); -- Dois vetores de entrada (Quatro bits)
+           s : out  STD_LOGIC_vector(3 downto 0); -- aqui são as saídas, S é um vetor de 4 bits
+           cout : out  STD_LOGIC); -- cout é o Carry Out
 end igual;
 
 architecture Behavioral of igual is
 	
 begin
-		s <= "0001" WHEN a = b
+		s <= "0001" WHEN a = b -- S receberá o vetor 0001 quando "a" for igual a "b", e 0000 caso contrário.
 		ELSE "0000";
 
 end Behavioral;
