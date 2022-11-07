0c8c9ec7c156e764e412e1b70d68f4d309a550df
rickauer <116601341+rickauer@users.noreply.github.com>
2022-11-07 18:05:08 -0300

    Add files via upload

--- subtracao.vhd
+++ subtracao.vhd
@@ -33,7 +33,7 @@ entity subtracao is
     Port ( cin : in  STD_LOGIC;
 			  a, b : in  STD_LOGIC_vector(3 downto 0);
            s : out  STD_LOGIC_vector(3 downto 0);
-           cout : out  STD_LOGIC);
+           cout,f3 : out  STD_LOGIC);
 
 end subtracao;
 architecture Behavioral of subtracao is
@@ -42,10 +42,10 @@ architecture Behavioral of subtracao is
 	END COMPONENT;
 	SIGNAL t: std_logic_vector(3 downto 1);
 begin
-	FA5: adder PORT MAP('1',a(0),NOT b(0),s(0),t(1));
-	FA6: adder PORT MAP(t(1),a(1),NOT b(1),s(1),t(2));
-	FA7: adder PORT MAP(t(2),a(2),NOT b(2),s(2),t(3));
-	FA8: adder PORT MAP(t(3),a(3),NOT b(3),s(3),Cout);
-
+	FA1: adder PORT MAP('1',a(0),NOT b(0),s(0),t(1));
+	FA2: adder PORT MAP(t(1),a(1),NOT b(1),s(1),t(2));
+	FA3: adder PORT MAP(t(2),a(2),NOT b(2),s(2),t(3));
+	FA4: adder PORT MAP(t(3),a(3),NOT b(3),s(3),Cout);
+	f3 <= t(3) ;
 end Behavioral;
 
