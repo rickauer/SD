be3e79c317c23a2ae0ed708938ff8d306358fdd9
rickauer <116601341+rickauer@users.noreply.github.com>
2022-11-07 17:59:09 -0300

    Update adder.vhd

--- adder.vhd
+++ adder.vhd
@@ -29,17 +29,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity adder is
-    Port ( x, y, z: in  STD_LOGIC;
-           Sum: out  STD_LOGIC;
-           Carry : out  STD_LOGIC);
+entity adder is --somador bit a bit 
+    Port ( x, y, z: in  STD_LOGIC; --aqui definimos as entradas, sendo x e y os bits a serem somados e z o carry in
+           Sum: out  STD_LOGIC; -- saída soma
+           Carry : out  STD_LOGIC); -- saída carry out
 end adder;
 
 architecture dataflow of adder is
 
 begin
-Sum<= x XOR y XOR z;
-Carry<=(y AND z) OR (y AND x) OR (z AND x);
+Sum<= x XOR y XOR z; -- aqui definimos a saída Sum, que recebe o valor de x XOR y XOR z
+Carry<=(y AND z) OR (y AND x) OR (z AND x); -- aqui definimos o carry de saída
 
 end dataflow;
 
