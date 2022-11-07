edf76d647d8d04f398ec7c840889dd6a4b261c7b
rickauer <116601341+rickauer@users.noreply.github.com>
2022-11-07 17:49:22 -0300

    Add files via upload

---
+++ decremento.vhd
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:32:28 10/18/2022 
+-- Design Name: 
+-- Module Name:    decremento - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decremento is
+    Port ( cin : in  STD_LOGIC;
+			  a, b : in  STD_LOGIC_vector(3 downto 0);
+           s : out  STD_LOGIC_vector(3 downto 0);
+           cout : out  STD_LOGIC);
+
+end decremento;
+architecture Behavioral of decremento is
+	component adder
+		PORT(x,y,z: IN std_logic; Sum, Carry: OUT std_logic);
+	END COMPONENT;
+	SIGNAL t: std_logic_vector(3 downto 1);
+begin
+	FA1: adder PORT MAP('0',a(0),'1',s(0),t(1));
+	FA2: adder PORT MAP(t(1),a(1),'1',s(1),t(2));
+	FA3: adder PORT MAP(t(2),a(2),'1',s(2),t(3));
+	FA4: adder PORT MAP(t(3),a(3),'1',s(3),Cout);
+
+end Behavioral;
+
+
